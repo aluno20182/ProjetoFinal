06af763ef960e04bb2255cb76a2e4d3e171fe484
aluno20182 <aluno20182@ipt.pt>
2019-12-08 11:46:14 +0000

    Update EnviarDados.js

--- EnviarDados.js
+++ EnviarDados.js
@@ -1,5 +1,5 @@
-import React from "react";
-import PropTypes from "prop-types";
+import React from 'react';
+import PropTypes from 'prop-types';
 import {
   Platform,
   Text,
@@ -8,15 +8,15 @@ import {
   ToastAndroid,
   ListView,
   StyleSheet,
-} from "react-native";
+  ScrollView,
+} from 'react-native';
 //Third party
-import Hotspot from "react-native-wifi-hotspot";
+import Hotspot from 'react-native-wifi-hotspot';
 import {Colors} from 'react-native/Libraries/NewAppScreen';
-
 
 class EnviarDados extends React.Component {
   static navigationOptions = {
-    title: "EnviarDados"
+    title: 'EnviarDados',
   };
   constructor(props) {
     super(props);
@@ -26,93 +26,151 @@ class EnviarDados extends React.Component {
     // console.warn("do Enable called");
     Hotspot.enable(
       () => {
-        ToastAndroid.show("Hotspot Enable", ToastAndroid.SHORT);
+        ToastAndroid.show('Hotspot Enable', ToastAndroid.SHORT);
       },
       err => {
         ToastAndroid.show(err.toString(), ToastAndroid.SHORT);
-      }
+      },
     );
   };
   //Disable HotsPot
   doDisable = () => {
     Hotspot.disable(
       () => {
-        ToastAndroid.show("Hotspot Disabled", ToastAndroid.SHORT);
+        ToastAndroid.show('Hotspot Disabled', ToastAndroid.SHORT);
       },
       err => {
         ToastAndroid.show(err.toString(), ToastAndroid.SHORT);
-      }
+      },
     );
   };
   //go to create screen
   goToCreate = () => {
-    this.props.navigation.navigate("CreateHotspot");
-  };
-  //fetch your hotspot settings.
-  // This funciton will give config details, after enable hotspot
-  doFetch() {
-    Hotspot.getConfig((config) => {
-      ToastAndroid.show(config.ssid, ToastAndroid.SHORT);
+    /* this.props.navigation.navigate('CreateHotspot'); */
+    var hotspot = { SSID: 'HelloWorld', password: 'helloworld' }
+    Hotspot.create(hotspot, () => {
+      ToastAndroid.show("Hotspot Created", ToastAndroid.SHORT);
     }, (err) => {
-      ToastAndroid.show(err.toString(), ToastAndroid.SHORT);
+      ToastAndroid.show("Deu merda!", ToastAndroid.SHORT);
     })
   };
+
+  //fetch your hotspot settings.
+  // This funciton will give config details, after enable hotspot
+  doFetch = () => {
+    Hotspot.getConfig(
+      config => {
+        ToastAndroid.show(config.ssid, ToastAndroid.SHORT);
+      },
+      err => {
+        console.log(config.ssid)
+
+        ToastAndroid.show(err.toString(), ToastAndroid.SHORT);
+      }
+    );
+  };
+
   //go to the peers screen
   goToPeers = () => {
-    this.props.navigation.navigate("Peers");
+    this.props.navigation.navigate('Peers');
   };
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Hotspot Library</Text>
-        <View style={styles.buttonView}>
-          <Text style={styles.subtitle}>
-            Enable & Check if it already opened
-          </Text>
-          <Button title="Enable" onPress={this.doEnable} />
+      <ScrollView>
+        <View style={styles.body}>
+          <View style={styles.sectionContainer}>
+            <View style={styles.instructionsContainer}>
+              <Text style={styles.highlight}>
+                Ativa e verifica se já está aberto
+              </Text>
+              <Button title="Ativar" onPress={this.doEnable} />
+            </View>
+            <View style={styles.instructionsContainer}>
+              <Text style={styles.highlight}>
+                Desativa e verifica se já está fechado
+              </Text>
+              <Button title="Desativar" onPress={this.doDisable} />
+            </View>
+            <View style={styles.instructionsContainer}>
+              <Text style={styles.highlight}>
+                Configura as opções do HotsPot
+              </Text>
+              <Button title="Criar" onPress={this.goToCreate} />
+            </View>
+            <View style={styles.instructionsContainer}>
+              <Text style={styles.highlight}>Fetch das opções de HotsPot</Text>
+              <Button title="Fetch" onPress={this.doFetch} />
+            </View>
+            <View style={styles.instructionsContainer}>
+              <Text style={styles.highlight}>Mostra todos os Peers</Text>
+              <Button title="Peers" onPress={this.goToPeers} />
+            </View>
+          </View>
         </View>
-        <View style={styles.buttonView}>
-          <Text style={styles.subtitle}>
-            Disable & Check if it already disabled
-          </Text>
-          <Button title="Disable" onPress={this.doDisable} />
-        </View>
-        <View style={styles.buttonView}>
-          <Text style={styles.subtitle}>Set your hotspot settings</Text>
-          <Button title="Create" onPress={this.goToCreate} />
-        </View>
-        <View style={styles.buttonView}>
-          <Text style={styles.subtitle}>Fetch your hotspot settings</Text>
-          <Button title="Fetch" onPress={this.doFetch} />
-        </View>
-        <View style={styles.buttonView}>
-          <Text style={styles.subtitle}>Show all Peers</Text>
-          <Button title="Peers" onPress={this.goToPeers} />
-        </View>
-      </View>
+      </ScrollView>
     );
   }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "flex-start",
-    margin: 8
+  scrollView: {
+    backgroundColor: Colors.lighter,
   },
-  welcome: {
-    fontSize: 20,
-    height: 60,
-    lineHeight: 50
+  engine: {
+    position: 'absolute',
+    right: 0,
   },
-  buttonView: {
-    marginBottom: 10
+  instructionsContainer: {
+    padding: 15,
+    borderBottomWidth: 1,
+    borderBottomColor: '#CCCCCC',
   },
-  subtitle: {
-    fontSize: 16,
-    fontWeight: "bold",
-    marginBottom: 5
-  }
+  body: {
+    backgroundColor: Colors.white,
+  },
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '600',
+    color: Colors.black,
+  },
+  sectionDescription: {
+    marginTop: 8,
+    fontSize: 18,
+    fontWeight: '400',
+    color: Colors.dark,
+  },
+  highlight: {
+    fontWeight: '700',
+  },
+  button: {
+    padding: 5,
+    width: 120,
+    alignItems: 'center',
+    backgroundColor: 'blue',
+    marginRight: 15,
+  },
+  bigButton: {
+    padding: 5,
+    width: 180,
+    alignItems: 'center',
+    backgroundColor: 'blue',
+    marginRight: 15,
+  },
+  buttonText: {
+    color: 'white',
+  },
+  footer: {
+    color: Colors.dark,
+    fontSize: 12,
+    fontWeight: '600',
+    padding: 4,
+    paddingRight: 12,
+    textAlign: 'right',
+  },
 });
 
 export default EnviarDados;
